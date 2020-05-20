ce486e474ae221d1ca1f418b08e3318601764da8
mmooyyii <moyishi213@gmail.com>
2020-05-20 22:59:47 +0800

    上天保佑没有bug

--- src/x_route.erl
+++ src/x_route.erl
@@ -11,7 +11,10 @@
 -type crit_bit_leaf() :: crit_bit_leaf().
 -type crit_bit_tree() :: crit_bit_tree().
 
--record(crit_bit_tree, {root :: crit_bit_node() | crit_bit_leaf()}).
+-record(crit_bit_tree, {
+    root :: crit_bit_node() | crit_bit_leaf(),
+    size :: integer()
+}).
 -record(crit_bit_node, {
     children :: {crit_bit_node()|crit_bit_leaf(), crit_bit_node()|crit_bit_leaf()},
     index :: integer(),
@@ -21,13 +24,20 @@
 -record(crit_bit_leaf, {key :: binary(), value :: term()}).
 
 -spec new() -> crit_bit_tree().
-new() -> #crit_bit_tree{root = ?NULL}.
+new() -> #crit_bit_tree{root = ?NULL, size = 0}.
 
 -spec insert(crit_bit_tree(), binary(), term()) -> crit_bit_tree().
 insert(#crit_bit_tree{root = ?NULL}, Key, Value) ->
-    #crit_bit_tree{root = new_leaf(Key, Value)};
-insert(#crit_bit_tree{}, _Key, _Value) ->
-    #crit_bit_tree{}.
+    #crit_bit_tree{root = new_leaf(Key, Value), size = 1};
+insert(#crit_bit_tree{root = Root, size = Size}, Key, Value) ->
+    NewRoot = check_node(Root, Key, Value),
+    case get(node) of
+        #crit_bit_node{} = Node ->
+            erase(),
+            #crit_bit_tree{root = add_node(Root, Node, Key), size = Size + 1};
+        undefined ->
+            #crit_bit_tree{root = NewRoot}
+    end.
 
 -spec lookup(crit_bit_tree(), binary()) -> {ok, term()} | {error, not_find}.
 lookup(#crit_bit_tree{}, _Key) ->
@@ -37,21 +47,48 @@ lookup(#crit_bit_tree{}, _Key) ->
 delete(#crit_bit_tree{}, _Key) ->
     ok.
 
-new_node(Index, Bit) ->
-    #crit_bit_node{index = Index, bit = Bit, children = {?NULL, ?NULL}}.
+check_node(#crit_bit_node{children = {C1, C2}} = Node, Key, Value) ->
+    case direction(Node, Key) of
+        1 -> Node#crit_bit_node{children = {check_node(C1, Key, Value), C2}};
+        2 -> Node#crit_bit_node{children = {C1, check_node(C2, Key, Value)}}
+    end;
 
-add_child(CN = #crit_bit_node{children = {_, C2}}, Node, 0) ->
-    CN#crit_bit_node{children = {Node, C2}};
-add_child(CN = #crit_bit_node{children = {C1, _}}, Node, 1) ->
-    CN#crit_bit_node{children = {C1, Node}}.
+check_node(#crit_bit_leaf{} = Leaf, Key, Value) ->
+    case critical_bit(Leaf, Key) of
+        {Index, Bit, -1} ->
+            Leaf#crit_bit_leaf{value = Value};
+        {Index, Bit, Cont} ->
+            Node = #crit_bit_node{children = Children} = new_node(Index, Bit, Cont),
+            Direction = direction(Node, Key),
+            put(node, Node#crit_bit_node{children = setelement(Direction, Children, new_leaf(Key, Value))})
+    end.
+
+
+add_node(#crit_bit_leaf{} = Node, #crit_bit_node{children = Children} = NewNode, Key) ->
+    Direction = direction(NewNode, Key),
+    NewNode#crit_bit_node{children = setelement(3 - Direction, Children, Node)};
+add_node(
+    #crit_bit_node{children = Children1, index = Index1, bit = Bit1} = Node,
+    #crit_bit_node{index = Index2, bit = Bit2, children = Children2} = NewNode, Key) ->
+    Direction = direction(NewNode, Key),
+    case Index1 > Index2 orelse (Index1 =:= Index2 andalso Bit1 < Bit2) of
+        true ->
+            NewNode#crit_bit_node{children = setelement(3 - Direction, Children2, Node)};
+        false ->
+            New = add_node(element(Direction, Children1), NewNode, Key),
+            Node#crit_bit_node{children = setelement(Direction, Children1, New)}
+    end.
+
+new_node(Index, Bit, Cont) ->
+    #crit_bit_node{cont = Cont, index = Index, bit = Bit, children = {?NULL, ?NULL}}.
 
 new_leaf(Key, Value) ->
     #crit_bit_leaf{key = Key, value = Value}.
 
 direction(#crit_bit_node{index = Index, bit = Bit, cont = Cont}, Key) ->
     case Index < length(Key) andalso (get_byte(Index, Key) band Bit orelse Cont) of
-        true -> 1;
-        false -> 0
+        true -> 2;
+        false -> 1
     end.
 
 critical_bit(#crit_bit_leaf{key = Key1}, Key2) when Key1 > Key2 ->
